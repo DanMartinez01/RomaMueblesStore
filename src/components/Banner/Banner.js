cddbd8894f2c910554efa3382f3b2526aa569f95
Daniela <martinezdl_@hotmail.com>
2021-08-08 13:19:23 -0300

    delete unused files

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Banner.css';
 
-import qualityIcon from '../Carousel/Img/quality.png';
+// import qualityIcon from '../Carousel/Img/quality.png';
 
 export const Banner = () => {
 
