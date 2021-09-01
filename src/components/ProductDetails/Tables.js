e39af3629433d5c447025283bde460fb7d6c35b8
Daniela <martinezdl_@hotmail.com>
2021-08-31 22:33:29 -0300

    add banner waves

--- src/components/ProductDetails/Tables.js
+++ src/components/ProductDetails/Tables.js
@@ -31,6 +31,7 @@ export const Tables = () => {
             <div className="bannerStripe">
                 <h1>Mesas de luz</h1>
             </div>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#E67E22" fill-opacity="1" d="M0,96L60,106.7C120,117,240,139,360,128C480,117,600,75,720,80C840,85,960,139,1080,154.7C1200,171,1320,149,1380,138.7L1440,128L1440,0L1380,0C1320,0,1200,0,1080,0C960,0,840,0,720,0C600,0,480,0,360,0C240,0,120,0,60,0L0,0Z"></path></svg>
             <div className="wrapper">
                 <div className="product">
                     <div className="card">
